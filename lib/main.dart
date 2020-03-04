0e71076aa258423d76de64d5e7846f176f7c2a04
mitali457 <mitalimondal450@gmail.com>
2020-03-04 18:36:51 +0530

    new app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:newapp/Screen/Login.dart';
+import 'package:newapp/FoodApp_Screen/Homescreen.dart';
+import 'package:newapp/FoodApp_Screen/Login.dart';
+import 'package:newapp/FoodApp_Screen/Registration.dart';
+import 'package:newapp/FoodApp_Screen/splash.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -8,7 +13,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Login(),
+      theme: ThemeData(
+        primaryColor: Colors.green,
+        accentColor: Colors.green,
+      ),
+      routes: {
+        '/login': (context) => Login(),
+        // When navigating to the "/second" route, build the SecondScreen widget.
+        '/registration': (context) => Registration(),
+      },
+      home: Login(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.onAuthStateChanged,
+      builder: (context,AsyncSnapshot<FirebaseUser> snapshot) {
+        if(snapshot.connectionState == ConnectionState.waiting)
+          return SplashPage();
+        if(!snapshot.hasData || snapshot.data == null)
+          return Login();
+        return Homescreen();
+      },
     );
   }
 }
